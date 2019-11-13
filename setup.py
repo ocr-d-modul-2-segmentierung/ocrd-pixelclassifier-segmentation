ced79c4cfa38d4a18571aa15cb33a8681987cf37
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2019-11-13 10:35:31 +0100

    Update setup.py for PyPI release

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Installs binaries:
-    - ocropus-gpageseg-with-coords
-    - ocrd-pc-seg-process
-    - ocrd-pc-seg-single
-"""
 import codecs
 
 from setuptools import setup, find_packages
@@ -18,20 +12,25 @@ setup(
     author='Alexander Gehrke, Christian Reul, Christoph Wick',
     author_email='alexander.gehrke@uni-wuerzburg.de, christian.reul@uni-wuerzburg.de, christoph.wick@uni-wuerzburg.de',
     url='https://github.com/ocr-d-modul-2-segmentierung/segmentation-runner',
-    license='Apache License 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=open("requirements.txt").read().split(),
     extras_require={
-        'tf_cpu': ['page-segmentation[tf_cpu]>=0.0.1'],
-        'tf_gpu': ['page-segmentation[tf_gpu]>=0.0.1'],
+        'tf_cpu': ['ocr4all_pixel_classifier[tf_cpu]>=0.0.1'],
+        'tf_gpu': ['ocr4all_pixel_classifier[tf_gpu]>=0.0.1'],
     },
     package_data={
         '': ['*.json', '*.yml', '*.yaml'],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering :: Image Recognition"
+
+    ],
     entry_points={
         'console_scripts': [
             'ocrd-pc-segmentation=ocrd_pc_segmentation.cli:ocrd_pc_segmentation',
         ]
     },
-    scripts=['ocrd_pc_segmentation/ocropus-gpageseg-with-coords'],
+    data_files=[('', ["requirements.txt"])],
 )
