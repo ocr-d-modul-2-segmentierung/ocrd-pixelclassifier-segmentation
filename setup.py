6a98e71b7f31ea61165ae312d48939a455a6b1e8
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2019-11-19 14:52:39 +0100

    Bump version and classifier dependency to v0.1.2
    - updates transitive opencv dependency to headless variant

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocrd_pc_segmentation',
-    version='0.1.1',
+    version='0.1.2',
     description='pixel-classifier based page segmentation',
     long_description=codecs.open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
