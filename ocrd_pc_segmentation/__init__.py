bf0a84d07e6e0d8b0520c1f7d4bd03891dab65f1
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2020-05-06 16:52:59 +0200

    Add a new default model
    The new default model is based on the LGT dataset (currently not
    publicly available because of unclear image rights). It consists mostly
    of Camerarius and Narrenschiff works (15th + 16th century).

--- ocrd_pc_segmentation/__init__.py
+++ ocrd_pc_segmentation/__init__.py
@@ -1,5 +1,9 @@
 import os
 
 DEFAULT_SEGMENTATION_MODEL_PATH = os.path.join(
+    os.path.dirname(__file__), 'lgt-model', 'model.h5'
+)
+
+LEGACY_SEGMENTATION_MODEL_PATH = os.path.join(
     os.path.dirname(__file__), 'dta2-model', 'model.h5'
 )
